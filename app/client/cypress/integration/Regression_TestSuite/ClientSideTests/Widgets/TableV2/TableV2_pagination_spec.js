20167a60a9ad4e5aed23f3ba563282e81305b9f2
Rajat Agrawal <rajat@appsmith.com>
2023-03-27 16:55:05 +0530

    Add cypress tests for previousPageVisited and nextPageVisited meta properties

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_pagination_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_pagination_spec.js
@@ -19,4 +19,46 @@ describe("Table Widget property pane feature validation", function () {
 
     cy.closePropertyPane();
   });
+
+  it("2. updates previous and next pagination propeties properly in non server side pagination mode", function () {
+    cy.openPropertyPane("tablewidgetv2");
+
+    // The text field has two bindings in its text value, as below
+    // "{{Table1.previousPageVisited}} {{Table1.nextPageVisited}}"
+
+    // Click on next page
+    cy.get(".t--table-widget-next-page").click();
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "false true");
+
+    // Click on previous page
+    cy.get(".t--table-widget-prev-page").click();
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "true false");
+
+    // Type and go to next page
+    cy.get(".t--table-widget-page-input .bp3-input").clear().type("2{enter}");
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "false true");
+
+    // Type and go to previous page
+    cy.get(".t--table-widget-page-input .bp3-input").clear().type("1{enter}");
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "true false");
+
+    cy.wait(15000);
+  });
+
+  it("3. updates previous and next pagination propeties properly in server side pagination mode", function () {
+    cy.openPropertyPane("tablewidgetv2");
+
+    cy.CheckWidgetProperties(commonlocators.serverSidePaginationCheckbox);
+
+    // The text field has two bindings in its text value, as below
+    // "{{Table1.previousPageVisited}} {{Table1.nextPageVisited}}"
+
+    // Click on next page
+    cy.get(".t--table-widget-next-page").click();
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "false true");
+
+    // Click on previous page
+    cy.get(".t--table-widget-prev-page").click();
+    cy.get(commonlocators.bodyTextStyle).should("have.text", "true false");
+  });
 });
